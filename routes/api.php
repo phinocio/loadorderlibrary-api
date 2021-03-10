0636d633b2bd2cee2730331868a7b77222314e67
Phinocio <github@phinocio.com>
2021-03-10 15:53:05 -0700

    add lists get and post routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoadOrderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/lists', [LoadOrderController::class, 'index'])->name('lists');
+Route::post('/lists', [LoadOrderController::class, 'store'])->name('lists.store');

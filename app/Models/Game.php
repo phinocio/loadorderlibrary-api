bff4e8729a851dbbd875f715100a4df696ad6e61
Phinocio <github@phinocio.com>
2021-03-10 15:53:55 -0700

    add models

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
-    use HasFactory;
+	use HasFactory;
+
+	public $timestamps = false;
+
+	public function loadOrders()
+	{
+
+	}
 }
--- app/Models/LoadOrder.php
+++ app/Models/LoadOrder.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class LoadOrder extends Model
 {
     use HasFactory;
+
+	protected $guarded = [];
 }

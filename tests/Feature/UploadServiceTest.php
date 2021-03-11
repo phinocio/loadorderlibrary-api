e67de6e245adbde5bd33e39ba4f17ea2b132aadb
Phinocio <github@phinocio.com>
2021-03-10 17:39:33 -0700

    add test for file uploading

---
+++ tests/Feature/UploadServiceTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Http\UploadedFile;
+use Storage;
+use App\Services\UploadService;
+
+class UploadServiceTest extends TestCase
+{
+	/** @test */
+	public function files_get_uploaded()
+	{
+		$files = [
+			UploadedFile::fake()->create('modlist.txt', 1),
+			UploadedFile::fake()->create('plugins.txt', 4),
+		];
+
+		$files = UploadService::uploadFiles($files);
+
+		Storage::disk('uploads')
+			->assertExists(explode(',', $files));
+
+		// Clean up the files
+		Storage::disk('uploads')->delete(explode(',', $files));
+	}
+}

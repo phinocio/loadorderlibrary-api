9e0e4f31e8d72890eae043f207d51601652b77ff
Phinocio <github@phinocio.com>
2021-07-05 19:52:04 -0600

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,8 +16,6 @@
 <body class="antialiased">
 	<nav></nav>
 
-	<script src="{{ mix('/js/manifest.js') }}"></script>
-	<script src="{{ mix('/js/vendor.js') }}"></script>
 	<script src="{{ mix('/js/app.js') }}"></script>
 </body>
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,7 +15,7 @@ require('laravel-mix-purgecss');
 mix.sass('resources/sass/app.scss', 'public/css').purgeCss().sourceMaps();
 
 // JS
-mix.js('resources/js/app.js', 'public/js').extract().sourceMaps();
+mix.js('resources/js/app.js', 'public/js').sourceMaps();
 
 mix.version();
 

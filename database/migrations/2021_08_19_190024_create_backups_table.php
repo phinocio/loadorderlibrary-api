40407d2493f7c439f6b30ef90b231e3521cfc97e
Phinocio <github@phinocio.com>
2021-08-31 12:44:04 -0600

    add migrations from main site code

---
+++ database/migrations/2021_08_19_190024_create_backups_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBackupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('backups', function (Blueprint $table) {
+            $table->id();
+			$table->string('file', 100)->unique();
+			$table->string('size', 50);
+			$table->timestamp('expires_at')->default(Carbon::now()->addDays(30));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('backups');
+    }
+}

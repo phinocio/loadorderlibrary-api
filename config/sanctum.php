1ae1e4ca6ccde1883db9ba3861a4ceac267dd960
Phinocio <github@phinocio.com>
2023-04-02 20:28:13 -0600

    add env var for local domains

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
 	'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
 		'%s%s%s',
-		'',
+		env('LOCAL_DOMAINS', ''),
 		env('APP_URL') ? ',' . parse_url(env('APP_URL'), PHP_URL_HOST) : '',
 		env('FRONTEND_URL') ? ',' . parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : '',
 	))),

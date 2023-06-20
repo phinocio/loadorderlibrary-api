ba3a3d33b15f266b2c9e7b17ce328542feef47c5
Phinocio <github@phinocio.com>
2023-06-20 12:03:35 -0600

    refactor: change ports/ips

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,9 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = [
+        '172.20.0.10'
+    ];
 
     /**
      * The headers that should be used to detect proxies.

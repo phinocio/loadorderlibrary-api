26f4dcb9b02ea13516bc3e11fdf5e0279a61e18e
Phinocio <github@phinocio.com>
2023-06-23 22:43:16 -0600

    fix: fix trust proxies by using the entire ip range

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -13,7 +13,7 @@ class TrustProxies extends Middleware
      * @var array<int, string>|string|null
      */
     protected $proxies = [
-        '172.20.0.10'
+        '172.20.0.1/24'
     ];
 
     /**

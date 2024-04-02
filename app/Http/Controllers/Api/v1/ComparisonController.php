9deda8dd1bdee2c517f2313f8362449ee1aca84a
Phinocio <github@phinocio.com>
2024-04-02 15:13:50 -0600

    feat: comparison diff route using diff command

--- app/Http/Controllers/Api/v1/ComparisonController.php
+++ app/Http/Controllers/Api/v1/ComparisonController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\v1\LoadOrderResource;
 use App\Models\LoadOrder;
+use Illuminate\Support\Facades\Storage;
 
 class ComparisonController extends Controller
 {
@@ -12,10 +14,35 @@ class ComparisonController extends Controller
     {
         $loadOrder1 = LoadOrder::where('slug', $loadOrder1)->with('files')->first();
         $loadOrder2 = LoadOrder::where('slug', $loadOrder2)->with('files')->first();
+
+        // Check for clean file names that exist in both lists
+        $loadOrder1Files = $loadOrder1->files->pluck('clean_name')->toArray();
+        $loadOrder2Files = $loadOrder2->files->pluck('clean_name')->toArray();
+        $inBoth = array_intersect($loadOrder1Files, $loadOrder2Files);
+
+        // Compare the files
+        $compare = [];
+        foreach ($inBoth as $file) {
+            $file1 = $loadOrder1->files->where('clean_name', $file)->first()?->name;
+            $file2 = $loadOrder2->files->where('clean_name', $file)->first()?->name;
+
+            if ($file1 === $file2) {
+                continue;
+            }
+
+            // The files are not the same, so diff them.
+            //            dd(shell_exec('diff -u' . Storage::disk('uploads')->path($file1) . ' ' . Storage::disk('uploads')->path($file2)));
+            $diff = shell_exec("/usr/bin/diff -u --label $file --label $file " . Storage::disk('uploads')->path($file1) . ' ' . Storage::disk('uploads')->path($file2));
+            array_push($compare, $diff);
+        }
+        //        dd($loadOrder1Files, $loadOrder2Files, $inBoth, $compare);
+
+
         return response()->json([
             'data' => [
-                'load_order1' => $loadOrder1->files,
-                'load_order2' => $loadOrder2->files
+                'load_order1' =>  new LoadOrderResource($loadOrder1),
+                'load_order2' => new LoadOrderResource($loadOrder2),
+                'diffs' => $compare
             ]
         ]);
     }

c0f81dfc0c51a0c974eba2beafb4e01d767d7ce6
Phinocio <github@phinocio.com>
2024-05-10 10:04:29 -0600

    tmp: need to see IP the reverse proxy sends

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
@@ -41,7 +42,9 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting(): void
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            Log::info('Remote: '.$_SERVER['REMOTE_ADDR'].', ReqIP: '.$request->ip());
+
+            return Limit::perMinute(120)->by($request->user()?->id ?: $request->ip());
         });
     }
 }

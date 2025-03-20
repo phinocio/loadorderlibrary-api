51da271554d919a18f8a192a06bb18e2b1fffef6
Phinocio <github@phinocio.com>
2025-03-19 22:04:59 -0600

    wip: auth

---
+++ app/Http/Controllers/v1/Auth/LoginController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\v1\Auth;
+
+class LoginController
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Invoke the class instance.
+     */
+    public function __invoke(): void
+    {
+        //
+    }
+}
--- routes/api/api.php
+++ routes/api/api.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+    require __DIR__.'/v1/auth.php';
+});

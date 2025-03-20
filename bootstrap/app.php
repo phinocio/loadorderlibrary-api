683ce3da3889cb28c6e6fe86fc64d50ee3cf4148
Phinocio <github@phinocio.com>
2025-03-19 20:29:43 -0600

    refactor: cleanup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,23 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Laravel\Sanctum\Http\Middleware\CheckAbilities;
+use Laravel\Sanctum\Http\Middleware\CheckForAnyAbility;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
+        apiPrefix: '',
+        api: __DIR__.'/../routes/api/api.php',
         commands: __DIR__.'/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->statefulApi();
+        $middleware->alias([
+            'abilities' => CheckAbilities::class,
+            'ability' => CheckForAnyAbility::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //

b9a75178c311ebaf36acb84ea4b4e296b184c9a4
Phinocio <github@phinocio.com>
2025-03-22 14:01:02 -0600

    feat: custom auth exception and middleware

--- app/Http/Controllers/v1/Auth/LoginController.php
+++ app/Http/Controllers/v1/Auth/LoginController.php
@@ -5,33 +5,25 @@ declare(strict_types=1);
 namespace App\Http\Controllers\v1\Auth;
 
 use App\Http\Requests\v1\LoginRequest;
+use App\Http\Resources\v1\UserResource;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController
 {
-    /** Create a new class instance. */
-    public function __construct()
-    {
-        //
-    }
-
-    /** Invoke the class instance. */
     public function __invoke(LoginRequest $request): JsonResponse
     {
         $validated = $request->validated();
 
-        if (Auth::check()) {
-            return response()->json(['message' => 'User already logged in']);
-        }
-
         if (Auth::attempt($validated)) {
             session()->regenerate();
 
-            return response()->json(['message' => 'Login successful']);
+            return response()->json([
+                'message' => 'Login successful',
+                'user' => new UserResource(Auth::user()),
+            ]);
         }
 
         return response()->json(['message' => 'Invalid credentials'], 401);
-
     }
 }

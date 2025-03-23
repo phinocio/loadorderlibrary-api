4afcefd1c82a38b4835816696a2387a401f918b7
Phinocio <github@phinocio.com>
2025-03-22 19:24:12 -0600

    feat: add route for getting current logged in user

---
+++ tests/Feature/v1/Auth/CurrentUserTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\User;
+
+it('returns the current authenticated user', function () {
+    $user = User::factory()->create();
+
+    login($user)
+        ->getJson('/v1/me')
+        ->assertOk()
+        ->assertJsonStructure([
+            'id',
+            'name',
+            'email',
+            'admin',
+            'verified',
+            'created',
+            'updated',
+        ])
+        ->assertJson([
+            'id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email,
+            'admin' => $user->is_admin,
+            'verified' => $user->is_verified,
+        ]);
+});
+
+it('returns a 401 when the user is not authenticated', function () {
+    $this->assertGuest();
+    $this->getJson('/v1/me')->assertUnauthorized();
+});

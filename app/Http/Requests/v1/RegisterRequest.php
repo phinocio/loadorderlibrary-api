3a27d1cf5fd1ea3f2206850600eb11c86fde8864
Phinocio <github@phinocio.com>
2025-03-22 19:12:46 -0600

    refactor: create user action

--- app/Http/Requests/v1/RegisterRequest.php
+++ app/Http/Requests/v1/RegisterRequest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Requests\v1;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class RegisterRequest extends FormRequest
@@ -17,7 +18,7 @@ class RegisterRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {

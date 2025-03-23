22df2ce23bb6e04a2043413681f5cb1bbc8464b3
Phinocio <github@phinocio.com>
2025-03-22 19:56:02 -0600

    refactor: rename action

--- app/Http/Controllers/v1/Auth/RegisterController.php
+++ app/Http/Controllers/v1/Auth/RegisterController.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\v1\Auth;
 
-use App\Actions\v1\User\CreateUserAction;
+use App\Actions\v1\User\CreateUser;
 use App\Http\Requests\v1\RegisterRequest;
 use App\Http\Resources\v1\UserResource;
 use Illuminate\Http\JsonResponse;
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Auth;
 class RegisterController
 {
     public function __construct(
-        private readonly CreateUserAction $createUser
+        private readonly CreateUser $createUser
     ) {}
 
     public function __invoke(RegisterRequest $request): JsonResponse

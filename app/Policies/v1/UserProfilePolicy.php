ae50ca51278e6874a3401d98104a0592af5aed11
Phinocio <github@phinocio.com>
2025-03-31 22:43:46 -0600

    refactor: remove unecessary comments

--- app/Policies/v1/UserProfilePolicy.php
+++ app/Policies/v1/UserProfilePolicy.php
@@ -8,13 +8,11 @@ use App\Models\User;
 
 final class UserProfilePolicy
 {
-    /** Determine whether the user can view the model. */
     public function view(?User $user, User $model): bool
     {
         return true;
     }
 
-    /** Determine whether the user can update the model. */
     public function update(User $user, User $model): bool
     {
         return $user->is($model);

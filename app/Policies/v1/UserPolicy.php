c279602262306e1b3133e0f308e8ece209ea9159
Phinocio <github@phinocio.com>
2025-03-31 22:43:46 -0600

    refactor: remove unecessary comments

--- app/Policies/v1/UserPolicy.php
+++ app/Policies/v1/UserPolicy.php
@@ -8,13 +8,11 @@ use App\Models\User;
 
 final class UserPolicy
 {
-    /** Determine whether the user can update the model. */
     public function update(User $user, User $model): bool
     {
         return $user->isAdmin() || $user->is($model);
     }
 
-    /** Determine whether the user can delete the model. */
     public function delete(User $user, User $model): bool
     {
         return $user->isAdmin() || $user->is($model);

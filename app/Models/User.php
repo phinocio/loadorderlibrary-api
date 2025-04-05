f28ba4ce3838e54b2ededfb809f60581cf161da2
Phinocio <github@phinocio.com>
2025-04-05 14:38:07 -0600

    chore: typings

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Models;
 
 use App\Observers\v1\UserObserver;
+use Carbon\CarbonInterface;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasOne;
@@ -12,6 +13,20 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @property-read int $id
+ * @property-read string $name
+ * @property-read string|null $email
+ * @property-read CarbonInterface|null $email_verified_at
+ * @property-read string $password
+ * @property-read string|null $remember_token
+ * @property-read bool $is_verified
+ * @property-read bool $is_admin
+ * @property-read string|null $verification_token
+ * @property-read CarbonInterface $created_at
+ * @property-read CarbonInterface $updated_at
+ * @property-read UserProfile|null $profile
+ */
 #[ObservedBy(UserObserver::class)]
 final class User extends Authenticatable
 {

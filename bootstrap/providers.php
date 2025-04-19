aa8189fd69097dceb24fc9415ff420f31b45f4d1
Phinocio <github@phinocio.com>
2025-04-18 22:27:06 -0600

    fix: remove uneeded telescope provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,9 +3,7 @@
 declare(strict_types=1);
 
 use App\Providers\AppServiceProvider;
-use App\Providers\TelescopeServiceProvider;
 
 return [
     AppServiceProvider::class,
-    TelescopeServiceProvider::class,
 ];

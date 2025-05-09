76a186fa31d98b6e619b032ec007c38ecd126f4b
Phinocio <github@phinocio.com>
2025-05-08 21:52:23 -0600

    refactor: transaction

--- app/Actions/v1/LoadOrder/CreateLoadOrder.php
+++ app/Actions/v1/LoadOrder/CreateLoadOrder.php
@@ -8,6 +8,7 @@ use App\Actions\v1\File\UploadFile;
 use App\Models\LoadOrder;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 final class CreateLoadOrder
 {
@@ -30,24 +31,26 @@ final class CreateLoadOrder
      */
     public function execute(array $data): LoadOrder
     {
-        $loadOrder = LoadOrder::query()->create([
-            'name' => $data['name'],
-            'description' => $data['description'] ?? null,
-            'version' => $data['version'] ?? null,
-            'website' => $data['website'] ?? null,
-            'discord' => $data['discord'] ?? null,
-            'readme' => $data['readme'] ?? null,
-            'is_private' => $data['is_private'] ?? false,
-            'expires_at' => $data['expires_at'] ?? null,
-            'user_id' => Auth::user()->id ?? null,
-            'game_id' => $data['game_id'],
-        ]);
+        return DB::transaction(function () use ($data) {
+            $loadOrder = LoadOrder::query()->create([
+                'name' => $data['name'],
+                'description' => $data['description'] ?? null,
+                'version' => $data['version'] ?? null,
+                'website' => $data['website'] ?? null,
+                'discord' => $data['discord'] ?? null,
+                'readme' => $data['readme'] ?? null,
+                'is_private' => $data['is_private'] ?? false,
+                'expires_at' => $data['expires_at'] ?? null,
+                'user_id' => Auth::user()->id ?? null,
+                'game_id' => $data['game_id'],
+            ]);
 
-        foreach ($data['files'] as $uploadedFile) {
-            $file = $this->uploadFile->execute($uploadedFile);
-            $loadOrder->files()->attach($file);
-        }
+            foreach ($data['files'] as $uploadedFile) {
+                $file = $this->uploadFile->execute($uploadedFile);
+                $loadOrder->files()->attach($file);
+            }
 
-        return $loadOrder->load(['files', 'author', 'game']);
+            return $loadOrder->load(['files', 'author', 'game']);
+        });
     }
 }

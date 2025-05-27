98eec59d7099baad90699e5a17156121570ab227
Phinocio <github@phinocio.com>
2025-05-26 20:59:28 -0600

    refactor: globally handle model 404s

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 use App\Http\Middleware\DenyAuthenticated;
 use App\Http\Middleware\EnsureUserIsAdmin;
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Laravel\Sanctum\Http\Middleware\CheckAbilities;
 use Laravel\Sanctum\Http\Middleware\CheckForAnyAbility;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -32,7 +35,16 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (NotFoundHttpException $e, $request) {
+            $previous = $e->getPrevious();
+
+            if ($previous instanceof ModelNotFoundException) {
+                return response()->json([
+                    'message' => str($previous->getModel())->afterLast('\\').' not found',
+                    'status' => Response::HTTP_NOT_FOUND,
+                ], Response::HTTP_NOT_FOUND);
+            }
+        });
     })->withSchedule(function (Schedule $schedule) {
         $schedule->command('auth:clear-resets')->everyFifteenMinutes();
         $schedule->command('lists:delete-expired')->everyMinute();

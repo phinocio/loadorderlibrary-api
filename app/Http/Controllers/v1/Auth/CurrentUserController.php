7a0d1519a0d63b90bc1487fb7c31865737a935ee
Phinocio <github@phinocio.com>
2025-05-26 18:39:31 -0600

    refactor: load lists with latest first

--- app/Http/Controllers/v1/Auth/CurrentUserController.php
+++ app/Http/Controllers/v1/Auth/CurrentUserController.php
@@ -5,15 +5,20 @@ declare(strict_types=1);
 namespace App\Http\Controllers\v1\Auth;
 
 use App\Http\Resources\v1\User\CurrentUserResource;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Facades\Auth;
 
 final class CurrentUserController
 {
     public function __invoke(): CurrentUserResource
     {
-        return new CurrentUserResource(Auth::user()?->load([
+        Auth::user()?->load([
             'profile',
-            'lists',
-        ]));
+            'lists' => function (HasMany $query) {
+                $query->orderBy('created_at', 'desc');
+            },
+        ]);
+
+        return new CurrentUserResource(Auth::user());
     }
 }

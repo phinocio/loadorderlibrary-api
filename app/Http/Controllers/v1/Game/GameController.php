d86d7fb4f847d0e4adee5eb6acc427a4fff8b6c9
Phinocio <github@phinocio.com>
2025-05-31 19:18:47 -0600

    feat: only show public lists

--- app/Http/Controllers/v1/Game/GameController.php
+++ app/Http/Controllers/v1/Game/GameController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers\v1\Game;
 use App\Enums\v1\CacheKey;
 use App\Http\Resources\v1\Game\GameResource;
 use App\Models\Game;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Facades\Cache;
 
@@ -16,7 +17,7 @@ final class GameController
     {
         $games = Cache::rememberForever(
             CacheKey::GAMES->value,
-            fn () => Game::query()->withCount('lists')->orderBy('name')->get()
+            fn () => Game::query()->withCount(['lists' => fn (HasMany $query) => $query->where('is_private', false)])->orderBy('name')->get()
         );
 
         return GameResource::collection($games);
@@ -26,7 +27,7 @@ final class GameController
     {
         $game = Cache::rememberForever(
             CacheKey::GAME->with($game),
-            fn () => Game::query()->withCount('lists')->where('slug', $game)->orWhere('name', $game)->firstOrFail()
+            fn () => Game::query()->withCount(['lists' => fn (HasMany $query) => $query->where('is_private', false)])->where('slug', $game)->orWhere('name', $game)->firstOrFail()
         );
 
         return new GameResource($game);

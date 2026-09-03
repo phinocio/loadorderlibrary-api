2567027bd0bfe54bb1f05d67a5b65e6cd278c2cc
Phinocio <github@phinocio.com>
2026-09-03 17:00:28 -0600

    refactor: add frontend url to app config

--- config/app.php
+++ config/app.php
@@ -55,6 +55,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
+    'frontend_url' => env('FRONTEND_URL', ''),
 
     /*
     |--------------------------------------------------------------------------
